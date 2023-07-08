44fd984c9124dac6514a6fa5753e279d4c20b3e6
barbajoe <sabe12070@gmail.com>
2023-07-08 12:52:03 -0500

    add and use `@barbajoe/svelte-lib`
    and the css-lib

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { Layout } from "@barbajoe/svelte-lib";
+  import type { HeaderLink, LogoData } from "@barbajoe/svelte-lib/dist/types";
+
+  $: links = [
+    { href: "/page1", text: "Page 1", active: $page.url.pathname === "/page1" },
+    { href: "/page2", text: "Page 2", active: $page.url.pathname === "/page2" },
+    { href: "/page3", text: "Page 3", active: $page.url.pathname === "/page3" },
+  ] as HeaderLink[];
+
+  const logo: LogoData = {
+    href: "/",
+    src: "/favicon.ico",
+    alt: "Logo",
+    ariaLabel: "go to landing page",
+    text: "barbajoe's svelte library",
+  };
+</script>
+
+<Layout {links} {logo}>
+  <slot />
+</Layout>
