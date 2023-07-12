ea8d9c5aeb5df757afa89fa4d31a28d405a4699d
barbajoe <sabe12070@gmail.com>
2023-07-11 21:05:34 -0500

    user preferences are now working!

--- src/lib/components/Header/UserPreferences.svelte
+++ src/lib/components/Header/UserPreferences.svelte
@@ -1,8 +1,38 @@
 <script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { Store } from "tauri-plugin-store-api";
+
+  import { onMount } from "svelte";
+
   import { Modal } from "$lib";
+  import type { Preferences } from "$types/Store";
   import SettingsIcon from "./SettingsIcon.svelte";
+  import { StoreConsts } from "$utils";
+
+  onMount(async () => {
+    preferences =
+      (await store.get(StoreConsts.preferences)) ||
+      ({
+        locale: await invoke("current_locale"),
+        showNotes: true,
+        showTimezone: true,
+        use24HourTime: true,
+      } as Preferences);
+  });
 
   let showModal = false;
+
+  let preferences: Preferences;
+
+  const store = new Store(StoreConsts.path);
+  const savePreferences = async () => {
+    saved = true; // optimistic update
+    await store.set(StoreConsts.preferences, preferences);
+    store.save();
+  };
+
+  // TODO: consider checking saved vs form data before setting saved to false
+  let saved = true;
 </script>
 
 <button
@@ -16,18 +46,40 @@
 <Modal bind:showModal>
   <svelte:fragment slot="dialog-header-text">User Preferences</svelte:fragment>
 
-  <div>
-    <p>TODO: add preferences</p>
+  <form on:change={() => (saved = false)}>
+    {#if preferences}
+      <label>
+        <span>Locale</span>
+        <input type="text" bind:value={preferences.locale} />
+      </label>
+
+      <label>
+        <input type="checkbox" bind:checked={preferences.use24HourTime} />
+        <span>Use 24 hour time</span>
+      </label>
+
+      <label>
+        <input type="checkbox" bind:checked={preferences.showTimezone} />
+        <span>Show timezone</span>
+      </label>
+
+      <label>
+        <input type="checkbox" bind:checked={preferences.showNotes} />
+        <span>Show notes</span>
+      </label>
+    {/if}
+  </form>
+
+  <div class="form-status" class:success={saved} class:error={!saved}>
+    {saved ? "Data Saved" : "Unsaved Changes"}
   </div>
 
   <svelte:fragment slot="dialog-footer">
     <button class="modal-button" on:click={() => (showModal = false)}>
-      cancel
+      close
     </button>
 
-    <button class="modal-button" on:click={() => (showModal = false)}>
-      save
-    </button>
+    <button class="modal-button" on:click={savePreferences}> save </button>
   </svelte:fragment>
 </Modal>
 
@@ -43,5 +95,35 @@
     border: 1px solid var(--color-link-text);
     padding: 0.3rem 0.5rem;
     border-radius: 0.5rem;
+  }
+
+  form {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+  form label {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  form label input[type="checkbox"] {
+    width: 1rem;
+    height: 1rem;
+  }
+  form label input[type="text"] {
+    width: 100%;
+  }
+
+  .form-status {
+    font-size: 2rem;
+    margin-top: 0.5rem;
+  }
+  .form-status.success {
+    color: var(--color-success-text);
+  }
+  .form-status.error {
+    color: var(--color-error-text);
   }
 </style>
