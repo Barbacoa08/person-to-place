b7df90c72f94774e3b378d3b963b6557f9e4a465
barbajoe <sabe12070@gmail.com>
2023-07-22 10:33:44 -0500

    add helper text about Locale's and improve styling

--- src/lib/components/Header/UserPreferences.svelte
+++ src/lib/components/Header/UserPreferences.svelte
@@ -70,7 +70,13 @@
             <label>
               <div>Locale</div>
               <input type="text" bind:value={preferences.locale} />
-              <div class="helper-text">TODO: make searchable dropdown</div>
+              <div class="helper-text">
+                Must be a valid <a
+                  href="https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes"
+                >
+                  ISO name
+                </a>
+              </div>
             </label>
 
             <label>
@@ -117,7 +123,7 @@
     grid-template-columns: 1fr 1fr;
     grid-gap: 1rem;
 
-    & div {
+    & > div {
       display: flex;
       flex-direction: column;
       gap: 1rem;
