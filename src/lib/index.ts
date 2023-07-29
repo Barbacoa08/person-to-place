607a3b56492200346b742f37a1bc8a6114c2dc7d
barbajoe <sabe12070@gmail.com>
2023-07-28 20:31:35 -0500

    use svelte-lib modal (#19)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,6 @@
 import InsertEntry from "./components/Header/InsertEntry.svelte";
 export { InsertEntry };
 
-import Modal from "./components/Modal.svelte";
-export { Modal };
-
 import TimezoneTable from "./components/TimezoneTable/Table.svelte";
 export { TimezoneTable };
 
