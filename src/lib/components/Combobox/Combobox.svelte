38eb95ca49423300f7b2d69d82080c736cf8e925
barbajoe <sabe12070@gmail.com>
2023-08-03 21:39:48 -0500

    working keyboard input!

--- src/lib/components/Combobox/Combobox.svelte
+++ src/lib/components/Combobox/Combobox.svelte
@@ -12,7 +12,9 @@
   export let place = "";
   export let timezone = "";
   export let required = false;
+
   const listboxId = `${id}-listbox`;
+  const listItemId = (index: number) => `listbox-item-${index}`;
 
   let activeOption: string | undefined = undefined; // TODO: active item id?
 
@@ -58,20 +60,28 @@
       bind:value={place}
       on:keydown={(event) => {
         if (["Tab", "Enter"].includes(event.key)) {
-          hoverIndex = 0;
-          if (isValidTimezone(items[0].value)) {
-            timezone = items[0].value;
-            place = items[0].text;
+          if (isValidTimezone(items[hoverIndex].value)) {
+            timezone = items[hoverIndex].value;
+            place = items[hoverIndex].text;
           } else {
             timezone = "";
             place = "";
           }
+          hoverIndex = 0;
         } else if (event.key === "Escape") {
           hoverIndex = 0;
           event.preventDefault();
           event.stopPropagation();
           timezone = "";
           place = "";
+        } else if (event.key === "ArrowDown") {
+          event.preventDefault();
+          event.stopPropagation();
+          hoverIndex = Math.min(hoverIndex + 1, items.length - 1);
+        } else if (event.key === "ArrowUp") {
+          event.preventDefault();
+          event.stopPropagation();
+          hoverIndex = Math.max(hoverIndex - 1, 0);
         } else {
           hoverIndex = 0;
         }
@@ -106,8 +116,8 @@
       <li
         class="active"
         role="option"
-        aria-selected={place === item.value}
-        id={`listbox-item-${item.value}`}
+        aria-selected={place === item.value || hoverIndex === index}
+        id={listItemId(index)}
         tabindex={hoverIndex === index ? 0 : -1}
         on:click={() => {
           timezone = item.value;
@@ -118,6 +128,19 @@
           if (event.key === "Enter") {
             timezone = item.value;
             place = item.text;
+          } else if (event.key === "ArrowDown") {
+            event.preventDefault();
+            event.stopPropagation();
+            hoverIndex = Math.min(hoverIndex + 1, items.length - 1);
+          } else if (event.key === "ArrowUp") {
+            event.preventDefault();
+            event.stopPropagation();
+            hoverIndex = Math.max(hoverIndex - 1, 0);
+          } else if (event.key === "Escape") {
+            event.preventDefault();
+            event.stopPropagation();
+            timezone = "";
+            place = "";
           }
         }}
       >
